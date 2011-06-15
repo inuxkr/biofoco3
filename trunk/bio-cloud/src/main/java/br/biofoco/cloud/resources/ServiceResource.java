d5eb5eef36262986b5043ed3e1ddeb4e3d165683
edward.ribeiro@gmail.com <edward.ribeiro@gmail.com@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2011-06-15 22:06:51 +0000

    yac

--- trunk/bio-cloud/src/main/java/br/biofoco/cloud/resources/ServiceResource.java
+++ trunk/bio-cloud/src/main/java/br/biofoco/cloud/resources/ServiceResource.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2011 University of Brasilia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package br.biofoco.cloud.resources;
 
 import java.io.IOException;
@@ -14,8 +29,15 @@ import br.biofoco.cloud.utils.JsonUtil;
 public class ServiceResource {
 	
 	private final ServiceManager manager = ServiceManager.getInstance();
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String greetings(){
+		return "Web service interface";
+	}
 
 	@GET
+	@Path("/services")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String listServices() throws IOException {
 		return JsonUtil.toString(manager.listServices());
