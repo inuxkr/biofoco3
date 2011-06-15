50f721a6771579213f4f53afbea05a33663c913b
edward.ribeiro <edward.ribeiro@gmail.com>
2011-06-15 21:14:33 +0000

    Reorganizing the files.

---
+++ bio-cloud/src/main/java/br/biofoco/cloud/core/Bootstrapper.java
@@ -0,0 +1,8 @@
+package br.biofoco.cloud.core;
+
+public class Bootstrapper {
+
+	public static void main(String[] args) {
+		
+	}
+}
--- bio-cloud/src/main/java/br/biofoco/cloud/core/JettyStarter.java
+++ bio-cloud/src/main/java/br/biofoco/cloud/core/JettyStarter.java
@@ -1,4 +1,4 @@
-package br.biofoco.cloud.utils;
+package br.biofoco.cloud.core;
 
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.servlet.Context;
@@ -8,25 +8,29 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.spi.container.servlet.ServletContainer;
 
-public class JettyStarter {
+public final class JettyStarter {
 	
+	private Server server;
 
-	public static void main(String[] args) throws Exception {
+	public void start(int port) throws Exception {
+		
         ServletHolder sh = new ServletHolder(ServletContainer.class);
         
         sh.setInitParameter("com.sun.jersey.config.property.resourceConfigClass", "com.sun.jersey.api.core.PackagesResourceConfig");
         sh.setInitParameter("com.sun.jersey.config.property.packages", "br.biofoco.cloud.resources");
 
-        Server server = new Server(9999);
+        server = new Server(port);
         Context context = new Context(server, "/", Context.SESSIONS);
         context.addServlet(sh, "/*");
         server.start();
         server.join();
-
-        Client c = Client.create();
-        WebResource r = c.resource("http://localhost:9999/");
-        System.out.println(r.get(String.class));
-
+	}
+	
+	public void stop() throws Exception {
         server.stop();
 	}
+	
+//    Client c = Client.create();
+//    WebResource r = c.resource("http://localhost:9999/");
+//    System.out.println(r.get(String.class));
 }
