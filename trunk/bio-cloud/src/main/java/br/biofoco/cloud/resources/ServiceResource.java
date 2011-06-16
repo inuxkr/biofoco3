590599c9732bbd58cd7d0a7571d027b4ad2dc1cf
edward.ribeiro@gmail.com <edward.ribeiro@gmail.com@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2011-06-16 00:51:30 +0000

    POST, GET, and file upload using JAX-RS.

--- trunk/bio-cloud/src/main/java/br/biofoco/cloud/resources/ServiceResource.java
+++ trunk/bio-cloud/src/main/java/br/biofoco/cloud/resources/ServiceResource.java
@@ -15,16 +15,32 @@
  */
 package br.biofoco.cloud.resources;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.UUID;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.apache.commons.io.IOUtils;
+
+import br.biofoco.cloud.services.ServiceInvocation;
 import br.biofoco.cloud.services.ServiceManager;
 import br.biofoco.cloud.utils.JsonUtil;
+
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.BodyPartEntity;
+import com.sun.jersey.multipart.FormDataParam;
+import com.sun.jersey.multipart.MultiPart;
 
 @Path("/")
 public class ServiceResource {
@@ -44,19 +60,95 @@ public class ServiceResource {
 		return JsonUtil.toString(serviceManager.listServices());
 	}
 	
-	@GET
-	@Path("/service/{id}")
+	@POST
+	@Path("/services/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String invokeService(@PathParam("id") String serviceID) {
+	public String invokeService(@PathParam("id") String serviceID, MultiPart multiPart) {
+		
+	    ServiceInvocation invocation = multiPart.getBodyParts().get(0).getEntityAs(ServiceInvocation.class);
+	    System.out.println("name : " + invocation.getFilename());
+		
 		return serviceManager.invokeService(serviceID);		
 	}
 	
 	@GET
 	@Path("/service/result/{task}")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getStatus(@PathParam("task") String taskID) {
-		
+	public String getStatus(@PathParam("task") String taskID) {		
 		return serviceManager.getTaskResult(taskID);
+	}
+		
+	@POST
+	@Path("/upload")
+	@Consumes("multipart/mixed")
+	public Response upload(MultiPart multiPart){
+		// get the second part which is the project logo
+	    BodyPartEntity bpe = (BodyPartEntity) multiPart.getBodyParts().get(0).getEntity();
+	    String id = UUID.randomUUID().toString();
+	    boolean isProcessed = false;
+	    String message = null;
+	    try {
+	      InputStream source = bpe.getInputStream();
+	      FileOutputStream fos = new FileOutputStream("/tmp/" + id);
+	      
+	      byte[] buf = new byte[1024 * 60];
+	      int c;
+	      
+	      while ((c = source.read(buf)) != -1) {
+	    	  fos.write(buf, 0, c);
+	      }
+	      fos.flush();
+	      fos.close();
+	   
+	      isProcessed = true;
+	 
+	    } catch (Exception e) {
+	      message = e.getMessage();
+	    }
+	    
+	    if (isProcessed) {
+	      return Response.status(Response.Status.ACCEPTED)
+	                     .entity("Attachements processed successfully.")
+	                     .type(MediaType.TEXT_PLAIN)
+	                     .build();
+	    }
+	    
+	    return Response.status(Response.Status.BAD_REQUEST)
+	                   .entity("Failed to process attachments. Reason : " + message)
+	                   .type(MediaType.TEXT_PLAIN)
+	                   .build();
+	}
+	
+    @POST
+    @Path("/upload2")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response handleUpload(@FormDataParam("file") InputStream stream) throws Exception {
+        return Response.ok(IOUtils.toString(stream)).build();
+    }	
+	
+	@POST
+	@Path("/upload3")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String create(@FormDataParam("file") InputStream file
+						,@FormDataParam("file") FormDataContentDisposition fileInfo
+						,@FormDataParam("name") String name
+						,@FormDataParam("description") String description) throws IOException {
+		
+		
+	    System.out.println(name);
+	    System.out.println(description);
+	    System.out.println(fileInfo.getFileName());
+	    
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(file));
+	    String line;
+	    
+	    while ((line = reader.readLine()) != null) {
+	    	System.out.println(line);
+	    }
+		
+		return "OK";
 	}
 	
 }
