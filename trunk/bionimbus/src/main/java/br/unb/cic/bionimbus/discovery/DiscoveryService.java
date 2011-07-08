f99c6d2147abf0250263051993342973ceaba895
hugovs@gmail.com <hugovs@gmail.com@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2011-07-08 14:29:21 +0000

    better thread implementation on plugins and services

--- trunk/bionimbus/src/main/java/br/unb/cic/bionimbus/discovery/DiscoveryService.java
+++ trunk/bionimbus/src/main/java/br/unb/cic/bionimbus/discovery/DiscoveryService.java
@@ -1,38 +1,62 @@
 package br.unb.cic.bionimbus.discovery;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import br.unb.cic.bionimbus.Service;
 import br.unb.cic.bionimbus.ServiceManager;
 
-public class DiscoveryService extends Thread implements Service {
+public class DiscoveryService implements Service, Callable<Boolean> {
+
+	private volatile boolean running = false;
+
+	private ExecutorService executorService = Executors.newCachedThreadPool();
 
 	public DiscoveryService(ServiceManager manager) {
-		super("DiscoveryService");
 		System.out.println("registering DiscoveryService...");
 		manager.register(this);
 	}
 
 	@Override
-	public void run() {
-		while (true) {
+	public Boolean call() {
+
+		running = true;
+
+		while (running) {
 			System.out.println("running DiscoveryService...");
+
+			// java.util.concurrent 1.5/1.6
+
+			// executors.submit(new Worker());
+
+			// aguardar nova requisicao com timeout
+
+			// pegar dados de todos os plugins via broadcast no P2P.
+
+			// se for getCloudInfo()
+			// retornar dados de todos os plugins consolidados
+
 			try {
-				sleep(5000);
+				Thread.sleep(5000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
+
+		return true;
 	}
 
 	@Override
 	public void start() {
 		System.out.println("starting DiscoveryService...");
-		super.start();
+		executorService.submit(this);
 	}
 
 	@Override
 	public void shutdown() {
-		// TODO Auto-generated method stub
-
+		running = false;
+		executorService.shutdownNow();
 	}
 
 	@Override
