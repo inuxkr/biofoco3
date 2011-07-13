20f6ffd17b5113d95f220a5b45e5dbcc7915d275
hugovs <hugovs@gmail.com>
2011-07-13 20:33:27 +0000

    Services now receive a P2P reference when starting

--- bionimbus/src/main/java/br/unb/cic/bionimbus/Service.java
+++ bionimbus/src/main/java/br/unb/cic/bionimbus/Service.java
@@ -1,8 +1,10 @@
 package br.unb.cic.bionimbus;
 
+import br.unb.cic.bionimbus.p2p.BioNimbusP2P;
+
 public interface Service {
 	
-	public void start();
+	public void start(BioNimbusP2P p2p);
 	
 	public void shutdown();
 	
