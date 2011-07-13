3e3ef3d20b6335a1f39c470fb3a912010a48eb68
hugovs <hugovs@gmail.com>
2011-07-13 20:49:45 +0000

    DiscoveryService now listens for P2P events.

--- bionimbus/src/main/java/br/unb/cic/bionimbus/discovery/DiscoveryService.java
+++ bionimbus/src/main/java/br/unb/cic/bionimbus/discovery/DiscoveryService.java
@@ -9,14 +9,18 @@ import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import br.unb.cic.bionimbus.Service;
 import br.unb.cic.bionimbus.ServiceManager;
 import br.unb.cic.bionimbus.p2p.BioNimbusP2P;
+import br.unb.cic.bionimbus.p2p.P2PEvent;
+import br.unb.cic.bionimbus.p2p.P2PListener;
 
-public class DiscoveryService implements Service, Callable<Boolean> {
+public class DiscoveryService implements Service, P2PListener, Callable<Boolean> {
 
 	private volatile boolean running = false;
 
 	private ExecutorService executorService = Executors
 			.newCachedThreadPool(new BasicThreadFactory.Builder()
 					.namingPattern("discoveryservice-%d").build());
+	
+	private BioNimbusP2P p2p;
 
 	public DiscoveryService(ServiceManager manager) {
 		System.out.println("registering DiscoveryService...");
@@ -52,11 +56,13 @@ public class DiscoveryService implements Service, Callable<Boolean> {
 	public void start(BioNimbusP2P p2p) {
 		System.out.println("starting DiscoveryService...");
 		executorService.submit(this);
+		p2p.addListener(this);
 	}
 
 	@Override
 	public void shutdown() {
 		running = false;
+		p2p.remove(this);
 		executorService.shutdownNow();
 	}
 
@@ -64,6 +70,12 @@ public class DiscoveryService implements Service, Callable<Boolean> {
 	public void getStatus() {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void onEvent(P2PEvent event) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
