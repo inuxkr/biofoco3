1f7836d84b51fdcb1ed7f20b6a5c922221150b34
edward.ribeiro <edward.ribeiro@gmail.com>
2011-08-13 17:03:53 +0000

    Nova versão do AsyncOp. Gostei do resultado. :)

--- bionimbus/src/main/java/br/unb/cic/bionimbus/p2p/rpc/AsyncOpTest.java
+++ bionimbus/src/main/java/br/unb/cic/bionimbus/p2p/rpc/AsyncOpTest.java
@@ -1,54 +1,40 @@
 package br.unb.cic.bionimbus.p2p.rpc;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+
+import com.google.common.collect.Sets;
 
 import junit.framework.TestCase;
 
 
 public class AsyncOpTest extends TestCase {
 	
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws Exception {
 				
 		AsyncOp<String> a = new AsyncOp<String>(
 		new Callable<String>() {
 
 			@Override
 			public String call() throws Exception {
+				TimeUnit.MINUTES.sleep(1);
 				System.out.println("executing");
 				return "OK";
 			}
 			
 		}
-		,new AsyncOpListener<String>() {
-			
-			@Override
-			public void onTimeoutException() {
-				System.out.println("tempo expirou!");
-			}
-			
-			@Override
-			public void onFinished(String result) {
-				System.out.println("Resultado: " + result);
-			}
-
-			@Override
-			public void onInterruptedException() {
-				System.out.println("interrupted exception");
-				
-			}
-		}
-//		,Executors.newCachedThreadPool()
+		,Executors.newCachedThreadPool()
 		, 3 * 1000);
 		
-		a.start();
+		a.execute();
+		
+		while (!Sets.newHashSet(AsyncOpStatus.FINISHED, AsyncOpStatus.TIMED_OUT).contains(a.getStatus())) {}
+		
+		System.out.println(Long.toString(a.elapsedTimeInMilliseconds()));
 		
 		System.out.println("async operation dispatched");
 		
-//		a.cancel();
-		
-		TimeUnit.SECONDS.sleep(2);				
-		a.completed("hello");
 		
 	}
 	
