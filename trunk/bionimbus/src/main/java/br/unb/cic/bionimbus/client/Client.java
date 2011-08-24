ab54d767a7081720bdd48ec6a2282ffd2064b58f
hugovs@gmail.com <hugovs@gmail.com@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2011-08-24 22:20:10 +0000

    Primeira versao de execucao de servicos com argumentos

--- trunk/bionimbus/src/main/java/br/unb/cic/bionimbus/client/Client.java
+++ trunk/bionimbus/src/main/java/br/unb/cic/bionimbus/client/Client.java
@@ -27,7 +27,7 @@ public class Client implements P2PListener {
 	public void startJob() {
 		JobInfo job = new JobInfo();
 		job.setId(null);
-		job.setArgs(null);
+		job.addArg("-R");
 		job.setServiceId(1023296285);
 		job.setInputs(null);
 		
--- trunk/bionimbus/src/main/java/br/unb/cic/bionimbus/plugin/hadoop/HadoopTask.java
+++ trunk/bionimbus/src/main/java/br/unb/cic/bionimbus/plugin/hadoop/HadoopTask.java
@@ -4,27 +4,36 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.concurrent.Callable;
 
+import br.unb.cic.bionimbus.client.JobInfo;
 import br.unb.cic.bionimbus.plugin.PluginService;
 import br.unb.cic.bionimbus.plugin.PluginTask;
 import br.unb.cic.bionimbus.plugin.PluginTaskState;
 
 public class HadoopTask implements Callable<PluginTask> {
+	
+	private JobInfo job = null;
 
 	private PluginTask task = null;
 	
 	private PluginService service = null;
 
-	public HadoopTask(PluginService service, PluginTask task) {
+	public HadoopTask(JobInfo job, PluginService service, PluginTask task) {
 		this.service = service;
 		this.task = task;
+		this.job = job;
 	}
 
 	@Override
 	public PluginTask call() throws Exception {
+		
+		String args = "";
+		for (String arg : job.getArgs()) {
+			args += (" " + arg);
+		}
 
 		Process p = null;
 		try {
-			p = Runtime.getRuntime().exec(service.getPath());
+			p = Runtime.getRuntime().exec(service.getPath() + args);
 			task.setState(PluginTaskState.RUNNING);
 			BufferedReader br = new BufferedReader(new InputStreamReader(
 					p.getInputStream()));
