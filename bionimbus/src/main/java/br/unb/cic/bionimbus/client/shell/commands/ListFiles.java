87841d2f853a9ebf41eb599329afa141d581221f
hugovs <hugovs@gmail.com>
2011-10-04 01:52:24 +0000

    Primeiro comando com utilizacao do P2P de maneira sincrona. Pode servir como base para os demais comandos.

--- bionimbus/src/main/java/br/unb/cic/bionimbus/client/shell/commands/ListFiles.java
+++ bionimbus/src/main/java/br/unb/cic/bionimbus/client/shell/commands/ListFiles.java
@@ -2,26 +2,50 @@ package br.unb.cic.bionimbus.client.shell.commands;
 
 import br.unb.cic.bionimbus.client.shell.Command;
 import br.unb.cic.bionimbus.client.shell.SimpleShell;
+import br.unb.cic.bionimbus.messaging.Message;
+import br.unb.cic.bionimbus.p2p.P2PEvent;
+import br.unb.cic.bionimbus.p2p.P2PEventType;
+import br.unb.cic.bionimbus.p2p.P2PListener;
+import br.unb.cic.bionimbus.p2p.P2PMessageEvent;
+import br.unb.cic.bionimbus.p2p.P2PMessageType;
+import br.unb.cic.bionimbus.p2p.P2PService;
+import br.unb.cic.bionimbus.p2p.messages.ListReqMessage;
+import br.unb.cic.bionimbus.p2p.messages.ListRespMessage;
+import br.unb.cic.bionimbus.plugin.PluginFile;
 
 public class ListFiles implements Command {
-	
+
 	public static final String NAME = "files";
-	
+
 	private final SimpleShell shell;
-	
+
 	public ListFiles(SimpleShell shell) {
 		this.shell = shell;
 	}
 
 	@Override
 	public String execute(String... params) throws Exception {
-		
+
 		if (!shell.isConnected())
-			throw new IllegalStateException("This command should be used with an active connection!");
+			throw new IllegalStateException(
+					"This command should be used with an active connection!");
+
+		P2PService p2p = shell.getP2P();
+		Communication comm = new Communication(p2p);
 
 		shell.print("Listing files...");
-		
-		return "";
+
+		comm.sendReq(new ListReqMessage(p2p.getPeerNode()),
+				P2PMessageType.LISTRESP);
+		ListRespMessage resp = (ListRespMessage) comm.getResp();
+
+		String list = "";
+		for (PluginFile file : resp.values()) {
+			list += "ID: " + file.getId() + "; NAME: " + file.getPath() + "; SIZE: " + file.getSize() + "\n";
+		}
+		list += "\n";
+
+		return list;
 	}
 
 	@Override
@@ -36,6 +60,54 @@ public class ListFiles implements Command {
 
 	@Override
 	public void setOriginalParamLine(String param) {
+	}
+
+	private class Communication implements Runnable, P2PListener {
+
+		private final P2PService p2p;
+		private Message reqMsg = null;
+		private Message respMsg = null;
+		private P2PMessageType respType = null;
+
+		public Communication(P2PService p2p) {
+			this.p2p = p2p;
+		}
+
+		@Override
+		public void onEvent(P2PEvent event) {
+			if (event.getType().equals(P2PEventType.MESSAGE)) {
+				P2PMessageEvent msgEvent = (P2PMessageEvent) event;
+				Message recvdMsg = msgEvent.getMessage();
+				if (recvdMsg.getType() == respType.code())
+					putResp(recvdMsg);
+			}
+		}
+
+		@Override
+		public void run() {
+			p2p.broadcast(reqMsg);
+		}
+
+		private synchronized void putResp(Message respMsg) {
+			p2p.remove(this);
+			this.respMsg = respMsg;
+			notify();
+		}
+
+		public synchronized Message getResp() throws InterruptedException {
+			while (respMsg == null)
+				wait();
+			return respMsg;
+		}
+
+		public void sendReq(Message reqMsg, P2PMessageType respType) {
+			this.reqMsg = reqMsg;
+			this.respType = respType;
+			p2p.addListener(this);
+			Thread t = new Thread(this);
+			t.start();
+		}
+
 	}
 
 }
