1c73b1ae68921240699573375dde1bec402026cd
calimaborges <calimaborges@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2011-10-07 23:34:59 +0000

    Alteracoes no escalonador. Adicionado mais uma variavel (uptime)

--- branches/carlos-sched/bionimbus/src/test/java/br/unb/cic/bionimbus/sched/AHPPolicyTest.java
+++ branches/carlos-sched/bionimbus/src/test/java/br/unb/cic/bionimbus/sched/AHPPolicyTest.java
@@ -10,15 +10,35 @@ import br.unb.cic.bionimbus.sched.policy.impl.AHPPolicy;
 public class AHPPolicyTest {
 
 	public static void main(String [] args) throws Exception {
-		comparePluginInfoTest();
+		//comparePluginInfoTest();
+		testAlgo();
+	}
+	
+	public static void testAlgo() throws SchedException {
+		List<PluginInfo> pis = new ArrayList<PluginInfo>();
+		for (int i = 0; i < 3; ++i) {
+			PluginInfo pi = new PluginInfo();
+			pi.setId(String.valueOf(i));
+			pi.setLatency(Math.round(Math.random() * 1000));
+			pi.setUptime(Math.round(Math.random() * 10));
+			pis.add(pi);
+		}
+		
+		PluginInfo pinfo = AHPPolicy.getBestService(pis);
+		System.out.println(pinfo.getId());
 	}
 	
 	public static void comparePluginInfoTest() throws SchedException {
 		List<PluginInfo> pis = new ArrayList<PluginInfo>();
 		for (int i = 0; i < 3; ++i) {
 			PluginInfo pi = new PluginInfo();
+			pi.setId(String.valueOf(i));
 			pi.setLatency(Math.round(Math.random() * 1000));
 			pis.add(pi);
+		}
+		
+		for (PluginInfo p: pis) {
+			p.setUptime(Math.round(Math.random() * 10));
 		}
 		
 		for (PluginInfo p : pis) {
