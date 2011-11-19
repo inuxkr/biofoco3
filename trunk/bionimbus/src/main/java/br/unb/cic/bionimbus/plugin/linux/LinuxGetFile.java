d4a0ed254463df6ab76a49c1f8792262d54e7905
hugovs@gmail.com <hugovs@gmail.com@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2011-11-19 19:36:14 +0000

    Copiando arquivo para nao apagar do storage.

--- trunk/bionimbus/src/main/java/br/unb/cic/bionimbus/plugin/linux/LinuxGetFile.java
+++ trunk/bionimbus/src/main/java/br/unb/cic/bionimbus/plugin/linux/LinuxGetFile.java
@@ -3,27 +3,30 @@ package br.unb.cic.bionimbus.plugin.linux;
 import java.io.File;
 import java.util.concurrent.Callable;
 
+import org.apache.commons.io.FileUtils;
+
 import br.unb.cic.bionimbus.p2p.Host;
 import br.unb.cic.bionimbus.plugin.PluginFile;
 import br.unb.cic.bionimbus.plugin.PluginGetFile;
 
 public class LinuxGetFile implements Callable<PluginGetFile> {
-	
+
 	private final PluginGetFile getFile;
-	
+
 	private final String serverPath;
-	
-	public LinuxGetFile(PluginFile pluginFile, String taskId, Host receiver, String serverPath) {
+
+	public LinuxGetFile(PluginFile pluginFile, String taskId, Host receiver,
+			String serverPath) {
 		getFile = new PluginGetFile();
 		getFile.setPeer(receiver);
 		getFile.setPluginFile(pluginFile);
 		getFile.setTaskId(taskId);
 		this.serverPath = serverPath;
 	}
-	
+
 	public PluginGetFile call() throws Exception {
-		File file = new File(LinuxGetInfo.PATH + File.separator + getFile.getPluginFile().getPath());
-		file.renameTo(new File(serverPath + File.separator + getFile.getPluginFile().getPath()));
+		FileUtils.copyFile(new File(LinuxGetInfo.PATH + File.separator + getFile.getPluginFile().getPath()),
+				new File(serverPath + File.separator + getFile.getPluginFile().getPath()));
 		return getFile;
 	}
 
