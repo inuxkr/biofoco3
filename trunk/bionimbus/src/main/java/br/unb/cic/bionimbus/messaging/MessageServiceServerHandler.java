424ddaab16b6198988f889dcd12380d9c8bd24be
hugovs@gmail.com <hugovs@gmail.com@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2011-11-20 21:25:19 +0000

    Solucao para que funcione com o NAT. E' necessario configurar port-forwarding no firewall.

--- trunk/bionimbus/src/main/java/br/unb/cic/bionimbus/messaging/MessageServiceServerHandler.java
+++ trunk/bionimbus/src/main/java/br/unb/cic/bionimbus/messaging/MessageServiceServerHandler.java
@@ -1,10 +1,14 @@
 package br.unb.cic.bionimbus.messaging;
 
+import java.net.InetSocketAddress;
+
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ChannelStateEvent;
 import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
+
+import br.unb.cic.bionimbus.p2p.messages.AbstractMessage;
 
 public class MessageServiceServerHandler extends SimpleChannelHandler {
 	
@@ -30,7 +34,11 @@ public class MessageServiceServerHandler extends SimpleChannelHandler {
 	@Override
 	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e)
 			throws Exception {
-		server.getMessageService().recvMessage((Message) e.getMessage());
+		InetSocketAddress addr = (InetSocketAddress) ctx.getChannel().getRemoteAddress();
+		AbstractMessage msg = (AbstractMessage) e.getMessage();
+		if (msg.getPeer() != null)
+			msg.getPeer().getHost().setAddress(addr.getAddress().getHostAddress());
+		server.getMessageService().recvMessage(msg);
 	}
 
 }
